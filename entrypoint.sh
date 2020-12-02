c3f742f206b3c03b444d7c069fec16204a64c89f
Juan Pablo Arias <juanpablo@arias.me>
2020-12-02 10:53:55 -0500

    [FIX] execute scirpts on /flyway folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ bash /pre-migration.sh
 flyway migrate
 flyway info
 
+cd /flyway/
 # Scripts post migration
 echo $INPUT_POST_MIGRATION_BASH_SHA | base64 -d > post_migration.sh
 bash post_migration.sh
