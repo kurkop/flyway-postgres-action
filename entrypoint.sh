4c2f74f1fc70fe7f8fdd1cb8a78b4ac20aac437f
Juan Pablo Arias <jpa3211@gmail.com>
2021-03-05 09:24:43 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo $INPUT_FLYWAY_CONF_SHA | base64 -d > /flyway/conf/flyway.conf
 
-cp $INPUT_FLYWAY_SQL/* /flyway/sql
+cp -rf $INPUT_FLYWAY_SQL/* /flyway/sql
 
 flyway info
 bash /pre-migration.sh
