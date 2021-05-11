c902f4c393926f05010ee164b91eebe2599d3d65
Juan Pablo Arias <jpa3211@gmail.com>
2021-05-11 11:12:33 -0500

    Fix sql folder (#1)
    Update entrypoint.sh and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,7 @@ FROM flyway/flyway:6.4.0
 
 USER root
 
-RUN apt-get update -y && apt-get install -y postgresql-client
-
-USER flyway
+RUN apt-get update -y && apt-get install -y postgresql-client libgdal-dev build-essential python3-pip python3-venv
 
 COPY entrypoint.sh /entrypoint.sh
 COPY pre-migration.sh /pre-migration.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,12 @@
 
 echo $INPUT_FLYWAY_CONF_SHA | base64 -d > /flyway/conf/flyway.conf
 
-cp -rf $INPUT_FLYWAY_SQL/* /flyway/sql
+echo "Workspace:"
+ls $GITHUB_WORKSPACE
+echo "SQL Folder:"
+ls $GITHUB_WORKSPACE/$INPUT_FLYWAY_SQL
+
+cp -rf $GITHUB_WORKSPACE/$INPUT_FLYWAY_SQL/* /flyway/sql/
 
 flyway info
 bash /pre-migration.sh
